34ee353831a2de983a439a6375ea26aaade8571e
SihamEdhiba <edhibasiham@gmail.com>
2024-07-12 19:39:32 +0100

    CHNAN

--- src/app/[lang]/[...not-found]/page.tsx
+++ src/app/[lang]/[...not-found]/page.tsx
@@ -1,30 +1,30 @@
-// Type Imports
-import type { Locale } from '@configs/i18n'
+// // Type Imports
+// import type { Locale } from '@configs/i18n'
 
-// Component Imports
-import Providers from '@components/Providers'
-import BlankLayout from '@layouts/BlankLayout'
-import NotFound from '@views/NotFound'
+// // Component Imports
+// import Providers from '@components/Providers'
+// import BlankLayout from '@layouts/BlankLayout'
+// import NotFound from '@views/NotFound'
 
-// Config Imports
-import { i18n } from '@configs/i18n'
+// // Config Imports
+// import { i18n } from '@configs/i18n'
 
-// Util Imports
-import { getServerMode, getSystemMode } from '@core/utils/serverHelpers'
+// // Util Imports
+// import { getServerMode, getSystemMode } from '@core/utils/serverHelpers'
 
-const NotFoundPage = ({ params }: { params: { lang: Locale } }) => {
-  // Vars
-  const direction = i18n.langDirection[params.lang]
-  const mode = getServerMode()
-  const systemMode = getSystemMode()
+// const NotFoundPage = ({ params }: { params: { lang: Locale } }) => {
+//   // Vars
+//   const direction = i18n.langDirection[params.lang]
+//   const mode = getServerMode()
+//   const systemMode = getSystemMode()
 
-  return (
-    <Providers direction={direction}>
-      <BlankLayout systemMode={systemMode}>
-        <NotFound mode={mode} />
-      </BlankLayout>
-    </Providers>
-  )
-}
+//   return (
+//     <Providers direction={direction}>
+//       <BlankLayout systemMode={systemMode}>
+//         <NotFound mode={mode} />
+//       </BlankLayout>
+//     </Providers>
+//   )
+// }
 
-export default NotFoundPage
+// export default NotFoundPage
